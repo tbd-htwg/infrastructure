34b23c1b652fff6fbd6728a794236633f08c4e19
Jakob Schwarz <jakob03schwarz@gmail.com>
2026-05-15 21:44:32 +0200

    Add recreate cluster script

---
+++ ms2/docs/recreate-cluster.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+
+# #Delete the cluster
+# gcloud container clusters delete tripplanning-gke \
+#   --region europe-west1 \
+#   --project milestone2-tbd-cad
+
+PROJECT="milestone2-tbd-cad"
+REGION="europe-west1"
+CLUSTER="tripplanning-gke"
+REPO_OWNER="tbd-htwg"
+REPO_NAME="infrastructure"
+REPO_BRANCH="main"
+REPO_PATH="ms2/gitops/clusters/dev"
+
+gcloud container clusters delete "$CLUSTER" \
+  --region "$REGION" \
+  --project "$PROJECT" \
+  --quiet
+
+cd /home/htwg/cloud-app-dev/infrastructure/ms2/terraform/envs/dev
+terraform apply -auto-approve
+
+gcloud container clusters get-credentials "$CLUSTER" \
+  --region "$REGION" \
+  --project "$PROJECT"
+
+flux bootstrap github \
+  --owner="$REPO_OWNER" \
+  --repository="$REPO_NAME" \
+  --branch="$REPO_BRANCH" \
+  --path="$REPO_PATH" \
+  --token-auth
